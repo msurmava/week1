74cbf325a1a0426e633f5a7bf1a4fa5e33f2dfb6
= <=>
2022-03-29 16:39:39 +0400

    number_9

---
+++ date/mydate.rb
@@ -0,0 +1,30 @@
+require "date"
+
+class Mydate
+  def initialize
+    puts "enter date" 
+    @day = gets.chomp().to_i
+    puts "enter month"
+    @month = gets.chomp().to_i
+    puts "enter year"
+    @year = gets.chomp().to_i
+  end
+  def date
+    number_of_days = Date.new(@year,@month,@day).yday.to_s
+  end
+  def orderal
+    if date[-1]  ==  "1" &&  date !=  "11"
+      return date  + "st"
+    elsif date[-1] ==  "2" &&  date !=  "12"
+      return  date  + "nd"
+    elsif date[-1] == "3"
+      return date + "rd"
+    else 
+      return date  + "th"
+    end    
+  end   
+   
+end
+
+
+puts Mydate.new.orderal
