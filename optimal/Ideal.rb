4abdd46dc032ed3110bbc059a6cfb2f158cbfe2e
= <=>
2022-03-29 15:03:28 +0400

    number_1

--- optimal/Ideal.rb
+++ optimal/Ideal.rb
@@ -1,5 +1,5 @@
 class Ideal_weight
-    TOSUBSTRACT = 100
+    TOSUBTRACT = 100
     EDGE  = 0
     def initialize
       puts  "Enter your name:"
@@ -8,7 +8,7 @@ class Ideal_weight
       @height = gets.chomp.to_i
     end
     def optimal
-      ideal = @height - TOSUBSTRACT
+      ideal = @height - TOSUBTRACT
       if ideal  < EDGE
         "#{@name.capitalize}, your weight is already optimal!"
       else
