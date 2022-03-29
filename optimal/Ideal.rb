306740bb117657c2c29a4979beae93bea2cda2ab
= <=>
2022-03-29 14:58:33 +0400

    number_1

--- optimal/Ideal.rb
+++ optimal/Ideal.rb
@@ -1,17 +1,18 @@
 class Ideal_weight
-    CONST = 100
-def initialize
-    puts "Enter your name:"
-    @name = gets.chomp
-    puts "Enter your heigtht"
-    @height = gets.chomp.to_f
-end
-def optimal  
-    ideal= @height - CONST
-    if ideal < 0
-    return "Your weight is already optimal"
-    else
-    return "#{@name.capitalize}, your ideal weight is #{ideal} kgs."
+    TOSUBSTRACT = 100
+    EDGE  = 0
+    def initialize
+      puts  "Enter your name:"
+      @name = gets.chomp
+      puts  "Enter your height"
+      @height = gets.chomp.to_i
     end
-end
+    def optimal
+      ideal = @height - TOSUBSTRACT
+      if ideal  < EDGE
+        "#{@name.capitalize}, your weight is already optimal!"
+      else
+        "#{@name.capitalize}, your ideal weight is #{ideal} kgs!"
+      end
+    end
 end
