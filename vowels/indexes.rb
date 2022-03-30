d66a298258b19f2da14dc7dc46e0e0f10214c17c
= <=>
2022-03-30 12:56:00 +0400

    eddit_8

--- vowels/indexes.rb
+++ vowels/indexes.rb
@@ -1,11 +1,13 @@
+#returns hash of vowels and their positions in alphabet.
 class Index 
+
   POSITION = 1
   
     def initialize
       @vowels
     end
 
-    def hash_with_indexes
+    def hash_of_vowels_with_indexes
       @vowels= { "a" => ("a".ord - "a".ord + POSITION),
         "e" => ("e".ord - "a".ord + POSITION),
         "i" => ("i".ord - "a".ord + POSITION),
@@ -15,4 +17,4 @@ class Index
     end
 end
 
- puts Index.new.hash_with_indexes
+ puts Index.new.hash_of_vowels_with_indexes
