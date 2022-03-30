accfb41f775752c98e6d8f5e1cea9b42a23e50c6
= <=>
2022-03-30 10:59:54 +0400

    eddit_2

--- area/triangle.rb
+++ area/triangle.rb
@@ -1,10 +1,16 @@
+#calculates area of a triangle by base and height
+
 class Triangle
   AREA = 1/2.to_f
+
   def initialize(base,height)
       @base = base
       @height = height
   end
+
   def area
     @base * @height * AREA
   end
 end
+
+puts Triangle.new(1,2).area
