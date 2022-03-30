1a6d83f372bf1f5e6ad3c5aa15aef7d5cab0138b
= <=>
2022-03-30 10:53:33 +0400

    eddit_1

--- optimal/Ideal.rb
+++ optimal/Ideal.rb
@@ -1,12 +1,14 @@
 class Ideal_weight
     TOSUBTRACT = 100
     EDGE  = 0
+
     def initialize
       puts  "Enter your name:"
       @name = gets.chomp
       puts  "Enter your height"
       @height = gets.chomp.to_i
     end
+    
     def optimal
       ideal = @height - TOSUBTRACT
       if ideal  < EDGE
