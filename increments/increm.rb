41d93b9b657c158dc6b113d2601b6decf36a071e
= <=>
2022-03-30 12:45:50 +0400

    eddit_5

--- increments/increm.rb
+++ increments/increm.rb
@@ -1,9 +1,12 @@
-class Increments
-  LIMIT = 100
-  NUMBER = 5
+#increments of five from 10 to 100
+LIMIT = 100
+NUMBER = 5
+
+class Increments  
   def initialize
     @numbers = [10]
   end
+  
   def of_5
     until @numbers[-1] == LIMIT
     @numbers << @numbers[-1] +  NUMBER
