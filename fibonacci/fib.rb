de8167f886911a1907b5f427800ce42f5c4eb832
= <=>
2022-03-30 12:52:48 +0400

    eddit_7

--- fibonacci/fib.rb
+++ fibonacci/fib.rb
@@ -1,14 +1,18 @@
+#fibonacci numbers up to 100
+LIMIT = 100
+
 class Fibonacci
   def initialize
     @fib_nums = [1,1]
   end
+
   def up_to_100
-    until @fib_nums[-1] > 100
+    until @fib_nums[-1] > LIMIT
       @fib_nums  <<  @fib_nums[-1]  + @fib_nums[-2]
     end
-    @fib_nums.delete(@fib_nums[-1])
-    @fib_nums
+    @fib_nums[0..-2]
   end
+
 end
 
 
