33ec69b09e25983a60a8b0f1d15a5bf2bf2ef246
= <=>
2022-04-01 12:55:53 +0400

    validation added

--- date/mydate.rb
+++ date/mydate.rb
@@ -11,11 +11,19 @@ class Mydate
     @year = gets.chomp().to_i
   end
 
+  def validate_parameters
+    if Date.valid_date?(@year,@month,@day) == false
+      warn "You entered date that is not valid, plase try again later."
+      exit 1
+    end
+  end
+
   def date
     number_of_days = Date.new(@year,@month,@day).yday.to_s
   end
   
   def days_in_order
+    validate_parameters
     if date[-1]  ==  "1" &&  date !=  "11"
       date  + "st"
     elsif date[-1] ==  "2" &&  date !=  "12"
