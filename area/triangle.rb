eeba5fe634775efe9bf0d67ddcfa68a7eb836063
= <=>
2022-04-01 12:39:42 +0400

    added validation

--- area/triangle.rb
+++ area/triangle.rb
@@ -4,13 +4,21 @@ class Triangle
   AREA = 1/2.to_f
 
   def initialize(base,height)
-    @base = base
-    @height = height
+    @base = base.to_f
+    @height = height.to_f
+  end
+
+  def validate_parameters
+    if @base <= 0 || @height  <=  0
+      warn "Parameters must be positive numbers!"
+      exit 1
+    end
   end
 
   def area
+    validate_parameters
     @base * @height * AREA
   end
 end
 
-puts Triangle.new(1,2).area
+puts Triangle.new(1,"0").area
