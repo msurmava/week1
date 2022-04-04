8ff87ff932fc1475b7f7f90c7f2c0f12314fe24d
= <=>
2022-04-04 10:35:02 +0400

    added method

--- optimal/Ideal.rb
+++ optimal/Ideal.rb
@@ -1,34 +1,37 @@
-#calculates ideal weight by height
-
+# calculates ideal weight by height
 class IdealWeightCounter
-    TOSUBTRACT = 100
-    EDGE  = 0
+  SUBTRACTOR = 110
+  EDGE = 0
 
-    def initialize
-      puts  "Enter your name:"
-      @name = gets.chomp
-      puts  "Enter your height"
-      @height = gets.chomp.to_i
-    end
+  def initialize
+    puts  'Enter your name:'
+    @name = gets.chomp
+    puts  'Enter your height'
+    @height = gets.chomp.to_i
+  end
 
-    def call
-      validate_parameters
-      ideal = @height - TOSUBTRACT
-      if ideal  < EDGE
-        "#{@name.capitalize}, your weight is already optimal!"
-      else
-        "#{@name.capitalize}, your ideal weight is #{ideal} kgs!"
-      end
+  def call
+    validate_parameters
+    calculate_ideal_weight
+    if @ideal < EDGE
+      "#{@name.capitalize}, your weight is already optimal!"
+    else
+      "#{@name.capitalize}, your ideal weight is #{@ideal} kgs!"
     end
+  end
 
-    private
-    def validate_parameters
-      if @height == 0
-        warn "#{@name.capitalize}, you entered invalid height"
-        exit 1
-      end
-    end
+  private
+
+  def calculate_ideal_weight
+    @ideal = @height - SUBTRACTOR
+  end
+
+  def validate_parameters
+    return unless @height.zero?
+
+    warn "#{@name.capitalize}, you entered invalid height"
+    exit 1
+  end
 end
-
 
 puts IdealWeightCounter.new.call
