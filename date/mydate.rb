cfcfed54077a67cf36aedeea43c654dae38b5a76
= <=>
2022-04-04 12:06:26 +0400

    added call

--- date/mydate.rb
+++ date/mydate.rb
@@ -1,40 +1,57 @@
-#returns how many days are from 1st of January to a specific date
-require "date"
+# frozen_string_literal: true
 
-class Mydate
+require 'date'
+# returns how many days are from 1st of January to a specific date
+class CountDaysFromJanuary1stToDate
   def initialize
-    puts "enter date" 
-    @day = gets.chomp().to_i
-    puts "enter month"
-    @month = gets.chomp().to_i
-    puts "enter year"
-    @year = gets.chomp().to_i
+    puts 'enter date'
+    @day = gets.chomp.to_i
+    puts 'enter month'
+    @month = gets.chomp.to_i
+    puts 'enter year'
+    @year = gets.chomp.to_i
   end
+
+  def call
+    validate_parameters
+    puts date_in_order_adds_st
+    puts date_in_order_adds_nd
+    puts date_in_order_adds_rd
+    puts date_in_order_adds_th
+  end
+
+  private
 
   def validate_parameters
-    if Date.valid_date?(@year,@month,@day) == false
-      warn "You entered date that is not valid, plase try again later."
-      exit 1
-    end
+    return unless Date.valid_date?(@year, @month, @day) == false
+
+    warn 'You entered date that is not valid, plase try again later.'
+    exit 1
   end
 
-  def date
-    number_of_days = Date.new(@year,@month,@day).yday.to_s
+  def date_to_string
+    @number_of_days = Date.new(@year, @month, @day).yday.to_s
   end
-  
-  def days_in_order
-    validate_parameters
-    if date[-1]  ==  "1" &&  date !=  "11"
-      date  + "st"
-    elsif date[-1] ==  "2" &&  date !=  "12"
-      date  + "nd"
-    elsif date[-1] == "3"
-      date + "rd"
-    else 
-      date  + "th"
-    end    
-  end   
+
+  def date_in_order_adds_st
+    date_to_string
+    "#{@number_of_days}st" if @number_of_days[-1] == '1' && @number_of_days != '11'
+  end
+
+  def date_in_order_adds_nd
+    date_to_string
+    "#{@number_of_days}nd" if @number_of_days[-1] == '2' && @number_of_days != '12'
+  end
+
+  def date_in_order_adds_rd
+    date_to_string
+    "#{@number_of_days}rd" if @number_of_days[-1] == '3'
+  end
+
+  def date_in_order_adds_th
+    date_to_string
+    "#{@number_of_days}th" unless date_in_order_adds_nd || date_in_order_adds_rd || date_in_order_adds_st
+  end
 end
 
-
-puts Mydate.new.days_in_order
+puts CountDaysFromJanuary1stToDate.new.call
