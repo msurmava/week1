210008d58bee157962ae49c84580a9bb18957daa
= <=>
2022-04-04 12:17:00 +0400

    order price sum

--- purchase/total.rb
+++ purchase/total.rb
@@ -1,29 +1,42 @@
-class Shop 
+# frozen_string_literal: true
+
+# returns list of products, quantity and price and sums prices.
+class OrderPriceCalculator
   def initialize
     @list = {}
     @price_quantitiy = []
   end
 
+  def call
+    puts plain_list
+    validate_parameters
+    puts one_item_at_a_time
+    puts "your total is #{total_sum}"
+  end
+
+  private
+
   def validate_parameters
-    if @price.to_f == 0 || @quantity.to_f == 0
-      warn "not valid parameters"
-      exit 1
-    end
+    return unless @price.to_f.zero? || @quantity.to_f.zero?
+
+    warn 'not valid parameters'
+    exit 1
   end
 
   def user_input
     loop do
-    puts "Please enter item:"
-    @name = gets.chomp()
-    break if @name.downcase == "stop"
-    puts "Please eneter price"
-    @price = gets.chomp()
-    puts "Please enter quantity:"
-    @quantity = gets.chomp()
-    @list[@name] = [@price.to_i,@quantity.to_i]
-    @price_quantitiy = [@price.to_i,@quantity.to_i]
+      puts 'Please enter item:'
+      @name = gets.chomp
+      break if @name.downcase == 'stop'
+
+      puts 'Please eneter price'
+      @price = gets.chomp
+      puts 'Please enter quantity:'
+      @quantity = gets.chomp
+      @list[@name] = [@price.to_i, @quantity.to_i]
+      @price_quantitiy = [@price.to_i, @quantity.to_i]
     end
-    @list 
+    @list
   end
 
   def plain_list
@@ -31,21 +44,12 @@ class Shop
   end
 
   def one_item_at_a_time
-    @list.each{|name,_| @list[name] = @price_quantitiy.inject(:*)}
+    @list.each { |name, _| @list[name] = @price_quantitiy.inject(:*) }
   end
 
   def total_sum
     one_item_at_a_time.values.inject(:+)
   end
-
-  def order_details
-    puts plain_list
-    validate_parameters
-    puts one_item_at_a_time
-    puts "your total is #{total_sum}"
-  end
 end
 
-
-
-Shop.new.order_details
+OrderPriceCalculator.new.call
