be5337de90af78798d62aa37639fc3b6b709acaf
= <=>
2022-04-04 11:33:16 +0400

    changed methods

--- equation/equation.rb
+++ equation/equation.rb
@@ -1,53 +1,62 @@
-FOR_DISC = 2
-TO_MULTIPLY = 4
-FOR_ROOT = 2
+# calcuates discriminant and roots for quadratic equation
+class QuadraticEquationCalculator
+  FOR_DISC = 2
+  TO_MULTIPLY = 4
+  FOR_ROOT = 2
 
-class Equation
-  
-  def initialize(first_coeff,second_coeff,third_coeff)
+  def initialize(first_coeff, second_coeff, third_coeff)
     @first_coeff = first_coeff.to_f
     @second_coeff = second_coeff.to_f
     @third_coeff = third_coeff.to_f
     @root = []
   end
 
+  def call
+    validate_parameters
+    discriminant_zero?
+    discriminant_positive?
+    discriminant_negative?
+  end
+
+  private
+
   def validate_parameters
-    if @first_coeff == 0 || @second_coeff ==  0 ||  @third_coeff  ==  0
-      warn  "parameters should not be 0!"
-      exit 1
-    end
+    return unless @first_coeff.zero? || @second_coeff.zero? || @third_coeff.zero?
+
+    warn 'parameters should not be 0!'
+    exit 1
   end
 
   def discriminant_calculation
-    @discriminant =  @second_coeff**FOR_DISC - TO_MULTIPLY*@first_coeff*@third_coeff
+    @discriminant = @second_coeff**FOR_DISC - TO_MULTIPLY * @first_coeff * @third_coeff
   end
 
-  def root_if_discriminant_zero
-    @root << (-@second_coeff  + Math.sqrt(@discriminant)) / FOR_ROOT * @first_coeff
+  def root_message
+    @root << (-@second_coeff + Math.sqrt(@discriminant)) / FOR_ROOT * @first_coeff
     puts "Discriminant is: #{@discriminant}, root for this equation is #{@root.join}"
   end
 
-  def root_if_discriminant_negative
-    puts "NEGATIVE DISCRIMINANT, equation has no ROOTS!"
+  def no_root_message
+    puts 'NEGATIVE DISCRIMINANT, equation has no ROOTS!'
   end
 
-  def roots_if_discriminant_positive
-    @root << (-@second_coeff  + Math.sqrt(@discriminant )) / FOR_ROOT * @first_coeff
-    @root << (-@second_coeff  - Math.sqrt(@discriminant )) / FOR_ROOT * @first_coeff
+  def roots_message
+    @root << (-@second_coeff + Math.sqrt(@discriminant)) / FOR_ROOT * @first_coeff
+    @root << (-@second_coeff - Math.sqrt(@discriminant)) / FOR_ROOT * @first_coeff
     puts "Discriminant is: #{@discriminant}, this equation has two roots : #{@root[0]} and #{@root[-1]}"
   end
-  
-  def calculate_roots
-    validate_parameters
-    if discriminant_calculation.zero?
-      root_if_discriminant_zero
-    elsif discriminant_calculation.positive?
-      roots_if_discriminant_positive
-    else
-      root_if_discriminant_negative
-    end 
+
+  def discriminant_zero?
+    root_if_discriminant_zero if discriminant_calculation.zero?
+  end
+
+  def discriminant_positive?
+    roots_message if discriminant_calculation.positive?
+  end
+
+  def discriminant_negative?
+    no_root_message if discriminant_calculation.negative?
   end
 end
 
-
-Equation.new(1,-14,0).calculate_roots
+QuadraticEquationCalculator.new(1, 4, 1).call
