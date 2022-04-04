b1fb343057f17592a4544270acda3a411e81e9f0
= <=>
2022-04-04 11:19:59 +0400

    changed methods

--- rectangular/triangle.rb
+++ rectangular/triangle.rb
@@ -1,64 +1,83 @@
 # Cheks what kind of triangle user has
+class WhatKindOfTriangle
+  POWER_2 = 2
+  ZERO = 0
 
-POWER_2 = 2
-TOCOMPARE = 0
-
-class Triangles
   def initialize
-    @sides  = []
-    puts  "Enter triangle side (cm):"
+    @sides = []
+    puts 'Enter triangle side (cm):'
     @sides << gets.chomp.to_i
-    puts  "Enter another triangle side (cm):"
+    puts 'Enter another triangle side (cm):'
     @sides << gets.chomp.to_i
-    puts  "Enter another triangle side (cm):"
+    puts 'Enter another triangle side (cm):'
     @sides << gets.chomp.to_i
     @sides.sort!.reverse!
   end
 
+  def call
+    validate_parameters
+    just_rectangular_message
+    just_isosceles_message
+    rectangular_and_isosceles_message
+    equilateral_and_isosceles_message
+    just_triangle_message
+    not_triangle_message
+  end
+
+  private
+
   def validate_parameters
-    if  @sides.include?(0)
-    warn "parameter not supported"
+    return unless @sides.include?(0)
+
+    warn 'parameter not supported'
     exit 1
-    end
   end
 
   def triangle?
-    compare_two_sides = @sides[0] < @sides[1] + @sides[2] 
-    side1_not_zero = @sides[0]  !=  TOCOMPARE 
-    side2_not_zero = @sides[1] !=  TOCOMPARE
-    side3_not_zero = @sides[2] !=  TOCOMPARE
-    compare_two_sides &&  side1_not_zero  &&  side2_not_zero  &&  side3_not_zero
+    compare_two_sides = @sides[0] < @sides[1] + @sides[2]
+    side1_not_zero = @sides[0] != ZERO
+    side2_not_zero = @sides[1] !=  ZERO
+    side3_not_zero = @sides[2] !=  ZERO
+    compare_two_sides &&  side1_not_zero && side2_not_zero && side3_not_zero
   end
 
   def rectangular?
-    @sides[0] **  POWER_2 == @sides[1]  **  POWER_2 + @sides[2] **  POWER_2
+    @sides[0]**POWER_2 == @sides[1]**POWER_2 + @sides[2]**POWER_2
   end
 
   def isosceles?
-    @sides[1] == @sides[2] || @sides[0] ==  @sides[1]
+    @sides[1] == @sides[2] || @sides[0] == @sides[1]
   end
 
   def equilateral?
-    @sides[0] ==  @sides[1] &&  @sides[1] ==  @sides[2]
+    @sides[0] == @sides[1] && @sides[1] == @sides[2]
   end
 
-  def kind_of_triangle
-    validate_parameters
-    if rectangular? && !isosceles?  &&  triangle?
-      puts  "Your triangle is rectangular!"
-    elsif rectangular? && isosceles?  &&  triangle?
-      puts "Your triangle is rectangular and isosceles!"
-    elsif isosceles? && !equilateral?  &&  triangle?
-      puts  "Your triangle is isosceles!"
-    elsif equilateral?  &&  triangle?
-      puts "Your triangle is equilateral and isosceles!"
-    elsif triangle?
-      puts "Your triangle is not equlateral, isosceler or rectangular!"
-    else 
-      puts "This is not a triangle"
+  def just_rectangular_message
+    puts  'Your triangle is rectangular!' if rectangular? && !isosceles? && triangle?
+  end
+
+  def rectangular_and_isosceles_message
+    puts  'Your triangle is rectangular and isosceles!' if rectangular? && isosceles? && triangle?
+  end
+
+  def just_isosceles_message
+    puts 'Your triangle is isosceles!' if isosceles? && triangle? && !rectangular? && !equilateral?
+  end
+
+  def equilateral_and_isosceles_message
+    puts 'Your triangle is equilateral and isosceles!' if equilateral? && triangle?
+  end
+
+  def just_triangle_message
+    if triangle? && !equilateral? && !rectangular? && !isosceles?
+      puts 'Your triangle is not equlateral, isosceler or rectangular!'
     end
+  end
+
+  def not_triangle_message
+    puts 'This is not a triangle' unless triangle?
   end
 end
 
-
-Triangles.new.kind_of_triangle
+WhatKindOfTriangle.new.call
