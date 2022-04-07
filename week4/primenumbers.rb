7018413447c4731eba1dc3fb2248e5ad344a22f4
= <=>
2022-04-07 11:31:05 +0400

    added file primenumbers

---
+++ week4/primenumbers.rb
@@ -0,0 +1,49 @@
+require 'prime'
+class AttributeError < StandardError; end
+
+# counts prime numebers
+class CountsPrimeNumberService
+  def initialize
+    @number = []
+  end
+
+  def call
+    user_input
+    validate_parameters
+    result_message
+  rescue AttributeError => e
+    warn e
+  end
+
+  private
+
+  def user_input
+    loop do
+      puts 'please eneter number (stop - to stop)'
+      @number << gets.chomp
+      break if @number[-1] == 'stop'
+    end
+  end
+
+  def count_primes
+    @number.select { |item| Prime.prime?(item.to_i) }.length
+  end
+
+  def result_message
+    "You entered #{count_primes} prime numbers"
+  end
+
+  def number?
+    @number[0..-2].all? { |item| item.match(/^(\d)+$/) }
+  end
+
+  def validate_parameters
+    return unless @number.empty? || !number?
+
+    raise AttributeError, 'please enter just numbers'
+  end
+end
+
+puts CountsPrimeNumberService.new.call
+
+
