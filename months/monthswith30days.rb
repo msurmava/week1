60ea926eec755eea0ecef25e66847fb3d1238339
= <=>
2022-04-08 16:15:39 +0400

    corrected

--- months/monthswith30days.rb
+++ months/monthswith30days.rb
@@ -1,19 +1,18 @@
 # shows days that have 30 days
 class MonthsWithThirtyDaysService
-  def initialize
-    @months = { 'January' => 31,
-                'February' => 28,
-                'March' => 31,
-                'April' => 30,
-                'May' => 31,
-                'June' => 30,
-                'July' => 31,
-                'August' => 31,
-                'September' => 30,
-                'October' => 31,
-                'November' => 30,
-                'December' => 31 }
-  end
+  DESIRED = 30
+  MONTHS = { 'January' => 31,
+             'February' => 28,
+             'March' => 31,
+             'April' => 30,
+             'May' => 31,
+             'June' => 30,
+             'July' => 31,
+             'August' => 31,
+             'September' => 30,
+             'October' => 31,
+             'November' => 30,
+             'December' => 31 }.freeze
 
   def call
     "Months with 30 days : #{days30}"
@@ -22,7 +21,7 @@ class MonthsWithThirtyDaysService
   private
 
   def days30
-    @months.select { |month, length| month if length == 30 }.keys.join(', ')
+    MONTHS.select { |month, length| month if length == DESIRED }.keys.join(', ')
   end
 end
 
