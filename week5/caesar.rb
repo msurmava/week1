2bf396021968bd579693b4322087039a160a5d3a
= <=>
2022-04-12 16:47:55 +0400

    cypher number changed'
    exit
    clear'

--- week5/caesar.rb
+++ week5/caesar.rb
@@ -1,25 +1,25 @@
-# Caesar Cypher encoder
+#caesaer cypher
 class CaesarCypherService
   class AttributeError < StandardError; end
 
-  CYPHER_SIZE = 5
-  SUBTRACTOR_FOR_LAST_FIVE = 26
-  CHAR_DOWNC_U = 123
-  UPC_LAST_FIVE_MIN = 90
-  UPC_LAST_FIVE_MAX = 96
-  UPC_MIN_EDGE = 69
-  UPC_MAX_EDGE = 90
-  DOWNC_MIN_EDGE = 101
-  DOWNC_MAX_EDGE = 122
+  FIRST_DOWNCASE = 96
+  LAST_DOWNCASE = 122
+  FIRST_UPCASE =  64
+  LAST_UPCASE = 90
+  SUBTRACTOR = 26
+  REGEX_FOR_NUMBER = /^(\d)+$/.freeze
 
   def initialize
     puts 'enter word'
     @word = gets.chomp
+    puts 'enter number'
+    @number = gets.chomp
+    @cypher_size = @number.to_i
   end
 
   def call
     validate_parameters
-    puts numbers_to_characters
+    numbers_to_characters
   rescue AttributeError => e
     warn e
   end
@@ -27,18 +27,18 @@ class CaesarCypherService
   private
 
   def char_to_num
-    @word.chars.map { |char| char.ord + CYPHER_SIZE }
+    @word.chars.map { |char| char.ord + @cypher_size }
   end
 
   def corrects_order
     char_to_num.map do |i|
-      i >= CHAR_DOWNC_U || (i > UPC_LAST_FIVE_MIN && i < UPC_LAST_FIVE_MAX) ? i - SUBTRACTOR_FOR_LAST_FIVE : i
+      i > LAST_DOWNCASE || (i > LAST_UPCASE && i <= LAST_UPCASE + @cypher_size) ? i - SUBTRACTOR : i
     end
   end
 
   def leaves_just_characters
     corrects_order.map do |i|
-      (i >= UPC_MIN_EDGE && i <= UPC_MAX_EDGE) || (i >= DOWNC_MIN_EDGE && i <= DOWNC_MAX_EDGE) ? i : i - CYPHER_SIZE
+      (i > FIRST_UPCASE && i <= LAST_UPCASE) || (i >= FIRST_DOWNCASE && i <= LAST_DOWNCASE) ? i : i - @cypher_size
     end
   end
 
@@ -46,13 +46,25 @@ class CaesarCypherService
     leaves_just_characters.map(&:chr).join
   end
 
+  def invalid_word
+    @word.empty? || !@word.is_a?(String)
+  end
+
+  def empty_num?
+    @number == ''
+  end
+
+  def valid_cypher_size
+    @number.scan(/\D/).empty?
+  end
+
   def validate_parameters
-    return unless @word.empty? || !@word.is_a?(String)
+    return unless invalid_word || !valid_cypher_size || empty_num?
 
     raise AttributeError, 'not valid parameters'
   end
 end
 
-CaesarCypherService.new.call
+puts CaesarCypherService.new.call
 
 
