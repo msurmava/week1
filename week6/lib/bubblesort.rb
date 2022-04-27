7d2af2161d8af98d95e028bb97f463979d4b7ac6
= <=>
2022-04-27 15:39:06 +0400

    changed variable names

--- week6/lib/bubblesort.rb
+++ week6/lib/bubblesort.rb
@@ -1,56 +1,55 @@
-# frozen_string_literal: true
-
-# Bubble sorts array of numbers
+# Bubble sorts array 
 class BublesortService
   class AttributeError < StandardError; end
 
   SUBTRACTOR = 1
   NEXT = 1
+  UNIQUE = 1
 
-  def initialize(number = [])
-    @numbers = number
+  def initialize(elements = [])
+    @elements = elements
     @sorted = []
   end
 
   def call
     validate_parameters
-    sort_numbers
+    sort_elements
   rescue AttributeError => e
     e.message
   end
+ 
+  private
 
-  # private
-
-  def numbers_length
-    @numbers.length
+  def elements_length
+    @elements.length
   end
 
-  def sort_numbers
+  def sort_elements
     loop do
       change_made = false
 
-      (numbers_length - SUBTRACTOR).times do |index|
-        if @numbers[index] > @numbers[index + NEXT]
-          @numbers[index], @numbers[index + NEXT] = @numbers[index + NEXT], @numbers[index]
+      (elements_length - SUBTRACTOR).times do |index|
+        if @elements[index] > @elements[index + NEXT]
+          @elements[index], @elements[index + NEXT] = @elements[index + NEXT], @elements[index]
           change_made = true
         end
       end
 
       break unless change_made
     end
-    @numbers
+    @elements
   end
 
   def same_type?
-    @numbers.map(&:class).uniq.length == 1
+    @elements.map(&:class).uniq.length == UNIQUE
   end
 
   def array_given?
-    @numbers.is_a?(Array)
+    @elements.is_a?(Array)
   end
 
   def invalid_elements
-    !same_type? || !array_given? || @numbers.empty?
+    !same_type? || !array_given? || @elements.empty?
   end
 
   def validate_parameters
@@ -58,4 +57,4 @@ class BublesortService
   end
 end
 
-p BublesortService.new({:ok => 5}).call
+BublesortService.new([1,2,3,1,2,5,25,6,56,7]).call
